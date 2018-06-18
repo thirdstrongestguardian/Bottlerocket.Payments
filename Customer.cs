a792a6c08af2ed80d1eedc36742b9a6c7fd6a5fc
William Hansen <william01@tutamail.com>
2018-06-18 09:42:19 -0700

    Started payments library.

---
+++ Customer.cs
@@ -0,0 +1,6 @@
+using System;
+
+namespace BottleRocket.Payments
+{
+	
+}
---
+++ MerchantAuthentication.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace BottleRocket.Payments
+{
+	public class MerchantAuthentication
+	{
+		public String Name { get; set; }
+		public String TransactionKey { get; set; }
+	}
+}
